9b45f5cd0e4d83dbeba54a8ec018c4d18d1b91c1
Hovander <ghovander@gmail.com>
2018-05-07 08:46:21 -0700

    Completed Lecture 49

--- Migrations/20180507154501_AddVehicle.Designer.cs
+++ Migrations/20180507154501_AddVehicle.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AspNetBasics.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180507154501_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
