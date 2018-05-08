97f186fff4cebd1c503ac91334e45a4108bb9f1a
Hovander <ghovander@gmail.com>
2018-05-08 11:13:11 -0700

    Complete Lectures through 53

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,24 @@
+using AspNetBasics.Controllers.Resources;
+using AspNetBasics.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetBasics.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mapper;
+        public VehiclesController(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            return Ok(vehicle);
+        }
+    }
+}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AspNetBasics.Controllers.Resources;
 using AspNetBasics.Models;
 using AutoMapper;
@@ -6,10 +7,19 @@ namespace AspNetBasics.Mapping
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile(){
+        public MappingProfile()
+        {
+            //Domain to API Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+
+            //API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
     }
 }
