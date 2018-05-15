952af2a790387437be2a14b2b8824c84203f04d4
Hovander <ghovander@gmail.com>
2018-05-15 09:03:27 -0700

    Completed Lecture 48

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -5,6 +5,7 @@ using AspNetBasics.Models;
 using AspNetBasics.Persistance;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetBasics.Controllers
 {
@@ -36,6 +37,22 @@ namespace AspNetBasics.Controllers
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
